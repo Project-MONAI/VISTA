15e37e32fb449c173dc04aa8d901b80b6ec80c64
Boris Fomitchev <bfomitchev@nvidia.com>
2024-08-05 12:44:04 -0700

    Using optional import
    Signed-off-by: Boris Fomitchev <bfomitchev@nvidia.com>

--- scripts/infer.py
+++ scripts/infer.py
@@ -33,11 +33,7 @@ from .sliding_window import point_based_window_inferer, sliding_window_inference
 from .train import CONFIG
 from .utils.trans_utils import VistaPostTransform, get_largest_connected_component_point
 
-try:
-    from monai.utils import TRTWrapper
-    TRT_AVAILABLE=True
-except Exception as e:
-    TRT_AVAILABLE=False
+from monai.utils import TRT_AVAILABLE, TRTWrapper
 
 rearrange, _ = optional_import("einops", name="rearrange")
 sys.path.insert(0, os.path.abspath(os.path.dirname(__file__)))
